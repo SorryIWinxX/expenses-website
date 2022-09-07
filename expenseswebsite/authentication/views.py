64676a33eb96b876f08753bd4d51f5c45111c6eb
Daniel <daniel2191937@correo.uis.edu.co>
2022-09-06 21:01:06 -0500

    VIDEO 9, 10
    falta arreglar el css ya que no lo carga cuando carga la pagina

--- expenseswebsite/authentication/views.py
+++ expenseswebsite/authentication/views.py
@@ -1,29 +1,33 @@
 from curses.ascii import isalnum
+import email
 from django.shortcuts import render
 from django.views import View
 import json
 import django.http 
 from json_response import JsonResponse
 from django.contrib.auth.models import User
+from validate_email import validate_email
 
 # Create your views here.
+
+class EmailValidationView(View):
+    def post(self, request):
+        data=json.loads(request.body)
+        email=data['email']
+        if not validate_email(email):
+            return JsonResponse({'email_error':'Email is invalid'},status=400)
+        if User.objects.filter(email = email).exists():
+            return JsonResponse({'email_error':'Sorry your email in use, please choose another email'},status=409)   
+        return JsonResponse({'email_valid':True})
+
 class UserNameValidationView(View):
     def post(self, request):
         data=json.loads(request.body)
         username=data['username']
-
-
         if not str(username).isalnum():
             return JsonResponse({'username_error':'username should only contains alphanumeric characters'},status=400)
-            
         if User.objects.filter(username = username).exists():
             return JsonResponse({'username_error':'Sorry your username in use, please choose another username'},status=409)   
-
-            
-            
-            
-            
-            
         return JsonResponse({'username_valid':True})
 
 class RegistrationView(View):
--- expenseswebsite/expenseswebsite/static/js/register.js
+++ expenseswebsite/expenseswebsite/static/js/register.js
@@ -1,8 +1,74 @@
 const usernameField = document.querySelector('#usernameField');
+const feedBackArea = document.querySelector('.invalid_feedback');
+const emailField = document.querySelector('#emailField');
+const emailFeedBackArea = document.querySelector('.email_FeedBack');
+const usernameSuccessOutput = document.querySelector('.usernameSuccessOutput');
+const emailSuccessOutput = document.querySelector('.emailSuccessOutput');
+const showPasswordToggle = document.querySelector('.showPasswordToggle');
+const passwordField =  document.querySelector('#passwordField');
+
+const handleToggleInput=(e)=>{
+
+    if(showPasswordToggle.textContent === 'SHOW'){
+
+        showPasswordToggle.textContent = 'HIDE';
+
+        passwordField.setAttribute("type","text");
+
+    }else{
+        showPasswordToggle.textContent = 'SHOW';
+        passwordField.setAttribute("type","password")
+    }
+
+}
+
+showPasswordToggle.addEventListener("click",handleToggleInput)
+
+
+
+emailField.addEventListener("keyup",(e)=>{
+
+    const emailValue = e.target.value;
+    emailSuccessOutput.style.display='block';
+    emailSuccessOutput.textContent=`Cheking ${emailValue}!`;
+
+
+    emailField.classList.remove("is-invalid");
+    emailFeedBackArea.style.display="none";
+
+    if(emailValue.length>0){
+
+
+        fetch("/authentication/validate-email",{
+            body:JSON.stringify({email: emailValue}),
+            method:"POST",
+
+         }).then(res=>res.json())
+         .then((data) =>{
+            console.log("data",data);
+            emailSuccessOutput.style.display='none';
+            if (data.email_error) {
+                emailField.classList.add("is-invalid");
+                emailFeedBackArea.style.display="block";
+                emailFeedBackArea.innerHTML=`<p>${data.email_error}</p>`
+            }
+         });
+
+    }
+
+
+})
+
+
 
 usernameField.addEventListener("keyup",(e)=>{
 
     const usernameValue = e.target.value;
+    usernameSuccessOutput.style.display='block';
+    usernameSuccessOutput.textContent=`Cheking ${usernameValue}!`
+
+    usernameField.classList.remove("is-invalid");
+    feedBackArea.style.display="none";
 
     if(usernameValue.length>0){
         fetch("/authentication/validate-username",{
@@ -12,8 +78,11 @@ usernameField.addEventListener("keyup",(e)=>{
          }).then(res=>res.json())
          .then((data) =>{
             console.log("data",data);
+            usernameSuccessOutput.style.display='none';
             if (data.username_error) {
                 usernameField.classList.add("is-invalid");
+                feedBackArea.style.display="block";
+                feedBackArea.innerHTML=`<p>${data.username_error}</p>`
             }
          });
 
